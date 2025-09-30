a40f9cd8690d870e8d6b1c294712c778d8ba3cc5
rick137-dev <caiulorick137@gmail.com>
2025-09-30 15:23:59 +0200

    Test commit

--- src/main/java/com/project/todoapp/HelloApplication.java
+++ src/main/java/com/project/todoapp/HelloApplication.java
@@ -7,6 +7,9 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
+//Test comment
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
